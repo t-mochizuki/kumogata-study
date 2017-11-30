b661055ed87639a6e0400e642bd40399267d2845
Mochizuki Takamasa <t-mochizuki@users.noreply.github.com>
2017-12-01 00:37:41 +0900

    Mappingsを使ってAMIを指定してスタックを作成してみた

--- my-stack.rb
+++ my-stack.rb
@@ -14,11 +14,24 @@ template do
     end
   end
 
+  Mappings do
+    AWSInstanceType2Arch(
+      {"t2.micro"=>{"Arch"=>"HVM64"}}
+    )
+    AWSRegionArch2AMI(
+      {"ap-northeast-1"=>{"HVM64"=>"ami-374db956"}}
+    )
+  end
+
   Resources do
     myEC2Instance do
       Type "AWS::EC2::Instance"
       Properties do
-        ImageId "ami-374db956"
+        ImageId do
+          Fn__FindInMap "AWSRegionArch2AMI", _{ Ref "AWS::Region" }, _{
+            Fn__FindInMap "AWSInstanceType2Arch", _{ Ref "InstanceType" }, "Arch"
+          }
+        end
         InstanceType { Ref "InstanceType" }
         KeyName "t-mochizuki"
 
